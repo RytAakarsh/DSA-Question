199ad3b1813f12b93cef683c9fa0f1bde34a6e24
RytAakarsh <aakarshmishra70@gmail.com>
2024-08-06 11:03:23 +0530

    function for finding area of cicle

---
+++ FXNAreacircle.cpp
@@ -0,0 +1,15 @@
+ //Function for finding Area of Circle
+ #include<iostream>
+ using namespace std ;
+ float areacircle ( int r )
+ {   float pi = 3.14;
+    return pi*r*r;
+ }
+ int main (){
+    int r;
+    cout<<"Enter your circle radius : ";
+    cin>>r;
+    float area = areacircle( r );
+    cout<<"Area of circle whose radius "<<r<<" is "<<area;
+    return 0;
+ }
